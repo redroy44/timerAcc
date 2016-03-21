8f4e426e71f803c51b334a1b01cbf04cf41e3529
Redroy44 <redroy44@gmail.com>
2016-03-21 22:26:10 +0100

    scalar kalman works

--- timerAcc.ino
+++ timerAcc.ino
@@ -4,17 +4,18 @@
 
 int calibrateOffsets = 1; // int to determine whether calibration takes place or not
 
-//const unsigned int oneSecInUsec = 1000000;  // A second in mirco second unit.
+const unsigned int oneSecInUsec = 1000000;  // A second in mirco second unit.
 
-const unsigned int time = 20000; //us -> 50Hz
+const unsigned int time = 10000; //us -> 50Hz
 // const unsigned int dt = 10000; // 100Hz
 
-const float dt = 0.02;
+const float dt = 0.1;
 
 unsigned int toggle = 0;
 
 Madgwick filter; // initialise Madgwick object
 float ax, ay, az;
+float axf, ayf, azf;
 int axRaw, ayRaw, azRaw;
 int gx, gy, gz;
 volatile float vx, vy, vz;
@@ -26,14 +27,36 @@ float roll;
 int factor = 800; // variable by which to divide gyroscope values, used to control sensitivity
 // note that an increased baud rate requires an increase in value of factor
 
-float convertRawAcceleration(int aRaw) {
+
+
+float convertRawAcceleration(int* aRaw) {
   // since we are using 2G range
   // -2g maps to a raw value of -32768
   // +2g maps to a raw value of 32767
 
-  float a = (aRaw * 2.0) / 32768.0;
+  float a = (*aRaw * 2.0) / 32768.0 * 9.80655;
 
   return a;
+}
+
+float kalmanFilter(float val) {
+  float w = pow(0.0001, 2);
+  float z = 0.000001;
+  static float P = w;
+  static float x = 0;
+  float error = 0.0;
+  float S = 0.0;
+  float K = 0.0;
+
+  x = x;
+  P = P + w;
+  error = val - x;
+  S = P + z;
+  K = P / S;
+  x = x + K * error;
+  P = P - (K * S * K);
+
+  return x;
 }
 
 void timedBlinkIsr()
@@ -53,23 +76,35 @@ void timedBlinkIsr()
   // read raw accelerometer measurements from device
   CurieIMU.readAccelerometer(axRaw, ayRaw, azRaw);
 
-  // convert the raw accelerometer data to G's and m/s^2
-  ax = convertRawAcceleration(axRaw) * 9.80665;
-  ay = convertRawAcceleration(ayRaw) * 9.80665;
-  az = convertRawAcceleration(azRaw) * 9.80665;
 
+  //Serial.print(ayRaw);
+  //Serial.print(","); // print comma so values can be parsed
+  //Serial.println(azRaw);
+
+  // convert the raw accelerometer data to G's and m/s^2
+  ax = convertRawAcceleration(&axRaw);
+  //ay = convertRawAcceleration(&ayRaw);
+  //az = convertRawAcceleration(&azRaw);
+
+  Serial.print(ax, 4);
+  Serial.print(","); // print comma so values can be parsed
+
+  axf = kalmanFilter(ax);
+  Serial.println(axf, 4);
 }
 
 void setup() {
   vx = vy = vz = 0;
   sx = sy = sz = 0;
 
-  Serial.begin(9600); // initialize Serial communication
+  Serial.begin(115200); // initialize Serial communication
   while (!Serial);    // wait for the serial port to open
 
   // Initialize pin 13 as an output - onboard LED.
   pinMode(13, OUTPUT);
 
+  Serial.print("Time dt: ");
+  Serial.println(dt, 2);
   // initialize device
   Serial.println("Initializing IMU device...");
   CurieIMU.begin();
@@ -112,37 +147,24 @@ void setup() {
 }
 
 void loop() {
-  if (CurieTimerOne.rdRstTickCount() > 0) {
-    vx += ax * dt; // m/s
-    vy += ay * dt;
-    //vz = 15;
-
-    sx += vx * dt * 100; // m
-    sy += vy * dt * 100; // m
-    //sz += vz * dt;
-  }
 }
 
 void serialEvent() {
   Serial.read();
-  noInterrupts();
-  Serial.print(ax, 4);
-  Serial.print(","); // print comma so values can be parsed
-  Serial.print(ay, 4);
-  Serial.print(","); // print comma so values can be parsed
-  Serial.print(az, 4);
-  Serial.print(","); // print comma so values can be parsed
-  Serial.print(vx, 4);
-  Serial.print(","); // print comma so values can be parsed
-  Serial.print(vy, 4);
-  Serial.print(","); // print comma so values can be parsed
-  Serial.print(vz, 4);
-  Serial.print(","); // print comma so values can be parsed
-  Serial.print(sx, 4);
-  Serial.print(","); // print comma so values can be parsed
-  Serial.print(sy, 4);
-  Serial.print(","); // print comma so values can be parsed
-  Serial.println(sz, 4);
-  interrupts();
+  //  noInterrupts();
+
+  //  Serial.print(","); // print comma so values can be parsed
+  //  Serial.print(vx, 4);
+  //  Serial.print(","); // print comma so values can be parsed
+  //  Serial.print(vy, 4);
+  //  Serial.print(","); // print comma so values can be parsed
+  //  Serial.print(vz, 4);
+  //  Serial.print(","); // print comma so values can be parsed
+  //  Serial.print(sx, 4);
+  //  Serial.print(","); // print comma so values can be parsed
+  //  Serial.print(sy, 4);
+  //  Serial.print(","); // print comma so values can be parsed
+  //  Serial.println(sz, 4);
+  //  interrupts();
 }
 
